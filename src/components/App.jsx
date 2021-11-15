ee39145d58d1a779476fd9374f6f9d993dee6713
Brycen Harmon <brycenharmon@gmail.com>
2021-11-14 21:41:32 -0600

    added TitleBar and connected it to App, also stylized a bit of App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
+import './App.css'
+import TitleBar from './TitleBar/TitleBar';
 
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title: "This is the first book title", author: "This is the first author"},
+            {title: "This is the second book", author: 'the second author'},
+            {title: "This is the third book", author: 'the third author'}
+        ];
+        this.state = {
+            bookNumber: 0
+        };
     }
 
     render(){
         return (
-            <h1>Oue react app from a component file</h1>
+            <div className="container-fluid">
+                <TitleBar/>
+                <div className="row">
+                    <div className="col-md-4">
+                        {/* {Button here to move to the previous book} */}
+                    </div>
+                    <div className="col-md-4">
+                        {/* {display book with cover} */}
+                        <h1>{this.books[this.state.bookNumber].title}</h1>
+                        <h4>{this.books[this.state.bookNumber].author}</h4>
+                    </div>
+                    <div className="col-md-4">
+                        {/* {Button here to move to the next book} */}
+                    </div>                
+                </div>
+            </div>
         )
     }
 }
